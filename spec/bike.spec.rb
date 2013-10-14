3990b0cd407ab2195a949031e3127c5b17e36b78
Abdi Ali <abdirazak.ali.12@live.com>
2013-10-14 15:01:13 +0100

    bike.spec.rb

--- spec/bike.spec.rb
+++ spec/bike.spec.rb
@@ -12,5 +12,18 @@ describe Bike do
 		#expect the method broken? of the object 'bike' to return false
 		expect(bike).not_to be_broken
 	end
+
+	it "should be able to break" do 
+		bike = Bike.new
+		bike.break
+		expect(bike).to be_broken
+	end
+
+	it "should be able to get fixed" do 
+		bike = Bike.new
+		bike.break
+		bike.fix
+		expect(bike).not_to be_broken
+	end
 end
 
