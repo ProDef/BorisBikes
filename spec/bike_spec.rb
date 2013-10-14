2cb806e074d0b6a4859fe0a60b3d0336a52a8991
Abdi Ali <abdirazak.ali.12@live.com>
2013-10-14 16:48:52 +0100

    refracted bike_spec

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,14 +4,16 @@ require '../lib/bike'
 
 #we're describing the functionality of a class, Bike
 describe Bike do 
+
+	let(:bike) {Bike.new}
 	#This is a specific feature (behaviour)
 	#that we expect to be present
-	it "should not be broken after we create it" do
-		bike = Bike.new # intialise a new object of bike class
+	#it "should not be broken after we create it" do
+		#bike = Bike.new # intialise a new object of bike class
 		# expect an instance of this class to have
 		#expect the method broken? of the object 'bike' to return false
-		expect(bike).not_to be_broken
-	end
+		#expect(bike).not_to be_broken
+	#end
 
 	it "should be able to break" do 
 		bike = Bike.new
