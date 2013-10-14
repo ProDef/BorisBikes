efdfd3712b4106bea4172a65ea2617738bb65ca4
Abdi Ali <abdirazak.ali.12@live.com>
2013-10-14 14:57:09 +0100

    ../lib/bike.rb

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,6 +1,24 @@
 class Bike
 
+	#the initialize method is always called when you create a new
+	#class by typipng Bike.new
+	def initialize
+		#all instance variables begin with "@"
+		#this must be an instance variable because we'll need it
+		#in other methods
+		@broken = false
+	end
+
 	def broken?
+		@broken
+	end
+
+	def break
+		@broken = true
+	end
+
+	def fix
+		@broken = false
 	end
 
 end
