744061af6a61d83ba4687e2b6b102abc1913d8fa
Abdi Ali <abdirazak.ali.12@live.com>
2013-10-17 11:30:17 +0100

    updated spec/van_spec.rb

---
+++ spec/van_spec.rb
@@ -0,0 +1,13 @@
+#collect broken bikes
+#return broken bikes
+#give broken bikes to garage
+#collect repaired bikes from garage
+#capacity 10
+#responding to to broken bike
+#respond to fix bike
+
+
+require 'van'
+
+describe Van do 
+	end
